fee1634f17574da8bc8901a84ecc584115a99b7f
Sangram <sangramraut7277@gmail.com>
2024-08-02 16:17:46 +0530

    Real Estate React

---
+++ Real-Estate/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import Header from './assets/components/Header'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+ <div className='app'>
+    <Header/>
+ </div>
+  )
+}
+
+export default App;
+
+
+
+
+
